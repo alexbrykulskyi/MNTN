d35348cb9d2f8ae0bcb83faacd9e954f3d10a576
Alex <alex.brykulskyi@gmail.com>
2022-04-29 12:52:11 +0300

    1

--- source/js/javascript.js
+++ source/js/javascript.js
@@ -5,10 +5,31 @@ $(document).ready(function() {
     $('.buttonToggle').toggleClass('buttonToggle--active'),
     $('.page').toggleClass('page--noScroll')
 
+
   });
 });
 
+var header=$("#head"),
+ introH = $("#head").innerHeight(),
+ scrollOffset =$(window).scrollTop();
 
+ /* Fixed Head */
+ checkScroll(scrollOffset);
+
+
+ $(window).on("scroll", function(){
+   scrollOffset = $(this).scrollTop();
+   checkScroll(scrollOffset);
+ });
+
+ function checkScroll(scrollOffset){
+   if(scrollOffset>=introH){
+   header.addClass("head--fixed");
+   }
+   else{
+     header.removeClass("head--fixed");
+   }
+  }
 
 
 
